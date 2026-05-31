61d59e97a4b2a084851c6b1e9e9eacbcaa4b7bc4
beden110299 <beden110299@github.com>
2026-05-31 20:11:53 +0700

    chore: remove (CTV) from sponsor code label in RegisterForm

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -134,7 +134,7 @@ export default function RegisterForm({ storeName, onRegisterSuccess, switchToLog
           </div>
 
           <div className="form-group">
-            <label className="form-label" htmlFor="register-sponsor">Mã bảo lãnh (CTV)</label>
+            <label className="form-label" htmlFor="register-sponsor">Mã bảo lãnh</label>
             <div className="input-wrapper">
               <span className="input-icon">
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
